30d627900c36c6b5d45260484f04cfc58da63db6
Adrian Castro <22133246+castdrian@users.noreply.github.com>
2024-02-23 20:01:24 +0100

    Initial Commit

---
+++ showdown/showdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  showdownApp.swift
+//  showdown
+//
+//  Created by Adrian Castro on 23.02.24.
+//
+
+import SwiftUI
+
+@main
+struct showdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
